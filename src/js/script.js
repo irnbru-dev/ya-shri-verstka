f74b35d39dea59ffe3faaa116cb0f3fcb9c94c86
Konovalova <darina.konovalova@it-grad.ru>
2018-08-21 18:31:15 +0300

    popups

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,32 @@
 $(document).ready(function () {
-    $(window).on('load resize' ,function() {
+    $(window).on('load resize', function () {
         if ($(window).width() < 960) {
             $('.main-nav').addClass('collapsed');
             $('.nav').removeClass('open')
         } else {
             $('.main-nav').removeClass('collapsed');
-        };
+        }
+        ;
     });
 
     $('.burger-btn').on('click', function () {
         $('.nav').toggleClass('open');
     });
-});
 
+    $('.card').on('click', function () {
+        var href = this.getAttribute("href");
+        var popupId = $('.popup').attr('id');
+        var popupIdWithAnchor = '#' + popupId;
+
+        console.log(href);
+        console.log(popupIdWithAnchor);
+
+        if (popupIdWithAnchor == href) {
+            $('.popup').addClass('open');
+        }
+    });
+
+    $('.popup__close-btn').on('click', function () {
+        $('.popup').removeClass('open');
+    });
+});
